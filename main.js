06243d4cc1841f3ba8ab9bb67da9c8fbc1c87965
soumdatta81 <soumdatta81@gmail.com>
2023-04-24 15:05:30 +0530

    v1 updates and fixes

--- main.js
+++ main.js
@@ -1,30 +1,54 @@
-var timeoutId;
-var endTime;
+let timeoutId;
+let endTime;
+
+const start_alert = new Audio('assests/gambare.mp3');
+const end_alert = new Audio('assests/onichan.mp3');
+const reset_alert = new Audio('assests/baka.mp3');
+
+//user input for the time
+// function startpomodoro(){
+//     let durationInput = document.getElementById("duration");
+//     let duration = parseInt(durationInput.value);
+//     if(!NaN(duration) && duration > 0){
+//         pomodoro(duration);
+//     }
+//     else{
+//         alert("Enter valid number like 69");
+//     }
+// }
 
 function pomodoro(mins) {
+  start_alert.play();
   clearTimeout(timeoutId); // Clear any previously scheduled updates
-  var duration = mins * 60 * 1000; // Duration in milliseconds
+  let duration = mins * 60 * 1000; // Duration in milliseconds
   endTime = new Date().getTime() + duration;
   updateTimerDisplay();
 }
 
+function take_break(mins){
+  clearTimeout(timeoutId); // Clear any previously scheduled updates
+  let duration = mins * 60 * 1000; // Duration in milliseconds
+  endTime = new Date().getTime() + duration;
+  updateTimerDisplay();
+}
 function resetTimer() {
+  reset_alert.play();
   clearTimeout(timeoutId); // Clear any previously scheduled updates
   endTime = null;
-  var timerDisplay = document.getElementById("timer");
+  let timerDisplay = document.getElementById("timer");
   timerDisplay.textContent = "00:00";
 }
 
 function updateTimerDisplay() {
   if (endTime !== null) {
-    var now = new Date().getTime();
-    var remainingTime = endTime - now;
+    let now = new Date().getTime();
+    let remainingTime = endTime - now;
 
     if (remainingTime >= 0) {
-      var remainingSeconds = Math.floor(remainingTime / 1000);
-      var minutes = Math.floor(remainingSeconds / 60);
-      var seconds = remainingSeconds % 60;
-      var timerDisplay = document.getElementById("timer");
+      let remainingSeconds = Math.floor(remainingTime / 1000);
+      let minutes = Math.floor(remainingSeconds / 60);
+      let seconds = remainingSeconds % 60;
+      let timerDisplay = document.getElementById("timer");
       timerDisplay.textContent =
         (minutes < 10 ? "0" + minutes : minutes) +
         ":" +
@@ -33,8 +57,8 @@ function updateTimerDisplay() {
       // Schedule the next update
       timeoutId = setTimeout(updateTimerDisplay, 100);
     } else {
+      end_alert.play();
       alert("Yay! You have successfully fo-cussed!");
-      resetTimer();
     }
   }
 }
